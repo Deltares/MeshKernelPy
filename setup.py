6e93a2d54c2591c9118aff83bf6d71d079a1c2ff
Julian Hofer <julianhofer@gnome.org>
2021-04-14 14:38:52 +0200

    Re-add accidentally deleted `setup.py`

---
+++ setup.py
@@ -0,0 +1,49 @@
+import sys
+from setuptools import find_namespace_packages, setup
+import codecs
+import os.path
+
+# edit author dictionary as necessary
+author_dict = {
+    "Julian Hofer": "Julian.Hofer@deltares.nl",
+    "Prisca van der Sluis": "prisca.vandersluis@deltares.nl",
+}
+__author__ = ", ".join(author_dict.keys())
+__author_email__ = ", ".join(s for _, s in author_dict.items())
+
+
+def read(rel_path):
+    here = os.path.abspath(os.path.dirname(__file__))
+    with codecs.open(os.path.join(here, rel_path), "r") as fp:
+        return fp.read()
+
+
+def get_version(rel_path):
+    for line in read(rel_path).splitlines():
+        if line.startswith("__version__"):
+            delim = '"' if '"' in line else "'"
+            return line.split(delim)[1]
+    else:
+        raise RuntimeError("Unable to find version string.")
+
+
+long_description = read("README.md")
+
+
+setup(
+    name="meshkernel",
+    description="`meshkernel` is a library which can be used to manipulate meshes.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author=__author__,
+    author_email=__author_email__,
+    url="https://github.com/Deltares/MeshKernelPy",
+    # TODO: specify license
+    license="",
+    platforms="Windows, Linux",
+    install_requires=["numpy"],
+    python_requires=">=3.7",
+    packages=find_namespace_packages(exclude=("tests", "examples")),
+    version=get_version("meshkernel/__init__.py"),
+    classifiers=["Topic :: Scientific/Engineering :: Mathematics"],
+)
