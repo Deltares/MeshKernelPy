d31228bc64796565268bbaa80523f015d52aca14
Julian Hofer <julianhofer@gnome.org>
2021-04-15 14:07:12 +0200

    GRIDEDIT-346: Extend documentation

--- meshkernel/py_structures.py
+++ meshkernel/py_structures.py
@@ -4,16 +4,16 @@ import numpy as np
 
 @dataclass
 class Mesh2d:
-    """This is a test class for dataclasses.
-
-    This is the body of the docstring description.
+    """This class is used for getting and setting two-dimensional mesh data
 
     Attributes:
+        # Read and write
         edge_nodes (np.ndarray(int)): The nodes composing each mesh 2d edge.
-        face_nodes (np.ndarray(int)): The nodes composing each mesh 2d face.
-        nodes_per_face (np.ndarray(int)): The nodes composing each mesh 2d face.
         node_x (np.ndarray(double)): The x-coordinates of the nodes.
         node_y (np.ndarray(double)): The y-coordinates of the nodes.
+        # Read-only
+        face_nodes (np.ndarray(int)): The nodes composing each mesh 2d face.
+        nodes_per_face (np.ndarray(int)): The nodes composing each mesh 2d face.
         edge_x (np.ndarray(double)): The x-coordinates of the mesh edges middle points.
         edge_y (np.ndarray(double)): The x-coordinates of the mesh edges middle points.
         face_x (np.ndarray(double)): The x-coordinates of the mesh faces mass centers.
