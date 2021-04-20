ef776970f9955f9d1ad7277effa23b3d6161a04e
Julian Hofer <julianhofer@gnome.org>
2021-04-20 11:50:24 +0200

    GRIDEDIT-352: Continue with Jupyter

--- meshkernel/py_structures.py
+++ meshkernel/py_structures.py
@@ -10,15 +10,15 @@ class Mesh2d:
     """This class is used for getting and setting two-dimensional mesh data
 
     Attributes:
-        node_x (np.ndarray(double)): The x-coordinates of the nodes.
-        node_y (np.ndarray(double)): The y-coordinates of the nodes.
-        edge_nodes (np.ndarray(int), optional): The nodes composing each mesh 2d edge.
-        face_nodes (np.ndarray(int), optional): The nodes composing each mesh 2d face.
-        nodes_per_face (np.ndarray(int), optional): The nodes composing each mesh 2d face.
-        edge_x (np.ndarray(double), optional): The x-coordinates of the mesh edges' middle points.
-        edge_y (np.ndarray(double), optional): The x-coordinates of the mesh edges' middle points.
-        face_x (np.ndarray(double), optional): The x-coordinates of the mesh faces' mass centers.
-        face_y (np.ndarray(double), optional): The y-coordinates of the mesh faces' mass centers.
+        node_x (np.ndarray): A 1D double array describing the x-coordinates of the nodes.
+        node_y (np.ndarray): A 1D double array describing the y-coordinates of the nodes.
+        edge_nodes (np.ndarray, optional): A 1D integer array describing the nodes composing each mesh 2d edge.
+        face_nodes (np.ndarray, optional): A 1D integer array describing the nodes composing each mesh 2d face.
+        nodes_per_face (np.ndarray, optional): A 1D integer array describing the nodes composing each mesh 2d face.
+        edge_x (np.ndarray, optional): A 1D double array describing x-coordinates of the mesh edges' middle points.
+        edge_y (np.ndarray, optional): A 1D double array describing x-coordinates of the mesh edges' middle points.
+        face_x (np.ndarray, optional): A 1D double array describing x-coordinates of the mesh faces' mass centers.
+        face_y (np.ndarray, optional): A 1D double array describing y-coordinates of the mesh faces' mass centers.
 
     """
 
