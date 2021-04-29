5ae2fcf9f49fef3676ac23a58c8ccef84b1e2a14
Prisca van der Sluis <prisca.vandersluis@deltares.nl>
2021-04-29 15:24:51 +0200

    GRIDEDIT-368 Add tests for py_structures default constructors.

--- tests/test_py_structures.py
+++ tests/test_py_structures.py
@@ -6,7 +6,11 @@ from numpy.testing import assert_array_equal
 from meshkernel import (
     AveragingMethod,
     DeleteMeshOption,
+    GeometryList,
     InterpolateToOption,
+    InterpolationParameters,
+    Mesh2d,
+    OrthogonalizationParameters,
     ProjectToLandBoundaryOption,
     RefinementType,
 )
@@ -20,6 +24,8 @@ cases_deletemeshoption_values = [
 
 @pytest.mark.parametrize("enum_val, exp_int", cases_deletemeshoption_values)
 def test_deletemeshoption_values(enum_val: DeleteMeshOption, exp_int: int):
+    """Tests the integer values of the `DeleteMeshOption` enum."""
+
     assert enum_val == exp_int
 
 
@@ -36,6 +42,8 @@ cases_projecttolandboundaryoption_values = [
 def test_projecttolandboundaryoption_values(
     enum_val: ProjectToLandBoundaryOption, exp_int: int
 ):
+    """Tests the integer values of the `ProjectToLandBoundaryOption` enum."""
+
     assert enum_val == exp_int
 
 
@@ -52,6 +60,8 @@ cases_averagingmethod_values = [
 
 @pytest.mark.parametrize("enum_val, exp_int", cases_averagingmethod_values)
 def test_averagingmethod_values(enum_val: AveragingMethod, exp_int: int):
+    """Tests the integer values of the `AveragingMethod` enum."""
+
     assert enum_val == exp_int
 
 
@@ -65,6 +75,8 @@ cases_interpolatetooption_values = [
 
 @pytest.mark.parametrize("enum_val, exp_int", cases_interpolatetooption_values)
 def test_interpolatetooption_values(enum_val: InterpolateToOption, exp_int: int):
+    """Tests the integer values of the `InterpolateToOption` enum."""
+
     assert enum_val == exp_int
 
 
@@ -77,4 +89,67 @@ cases_refinementtype_values = [
 
 @pytest.mark.parametrize("enum_val, exp_int", cases_refinementtype_values)
 def test_refinementtype_values(enum_val: RefinementType, exp_int: int):
+    """Tests the integer values of the `RefinementType` enum."""
+
     assert enum_val == exp_int
+
+
+def test_mesdh2d_constructor():
+    """Tests the default values after constructing a `Mesh2d`."""
+
+    node_x = np.array([0.0, 1.0, 1.0, 0.0], dtype=np.double)
+    node_y = np.array([0.0, 0.0, 1.0, 1.0], dtype=np.double)
+    edge_nodes = np.array([0, 1, 1, 2, 2, 3, 3, 0], dtype=np.int32)
+
+    mesh2d = Mesh2d(node_x, node_y, edge_nodes)
+
+    assert mesh2d.node_x.size == 4
+    assert mesh2d.node_y.size == 4
+    assert mesh2d.edge_nodes.size == 8
+    assert mesh2d.face_nodes.size == 0
+    assert mesh2d.nodes_per_face.size == 0
+    assert mesh2d.edge_x.size == 0
+    assert mesh2d.edge_y.size == 0
+    assert mesh2d.face_x.size == 0
+    assert mesh2d.face_y.size == 0
+
+
+def test_geometrylist_constructor():
+    """Tests the default values after constructing a `GeometryList`."""
+
+    x_coordinates = np.array([0.0], dtype=np.double)
+    y_coordinates = np.array([1.0], dtype=np.double)
+    geometry_list = GeometryList(x_coordinates, y_coordinates)
+
+    assert geometry_list.x_coordinates.size == 1
+    assert geometry_list.y_coordinates.size == 1
+    assert geometry_list.values.size == 0
+    assert geometry_list.geometry_separator == -999.0
+    assert geometry_list.inner_outer_separator == -998.0
+
+
+def test_orthogonalizationparameters_constructor():
+    """Tests the default values after constructing a `OrthogonalizationParameters`."""
+
+    parameters = OrthogonalizationParameters()
+
+    assert parameters.outer_iterations == 2
+    assert parameters.boundary_iterations == 25
+    assert parameters.inner_iterations == 25
+    assert parameters.orthogonalization_to_smoothing_factor == 0.975
+    assert parameters.orthogonalization_to_smoothing_factor_at_boundary == 1.0
+    assert parameters.areal_to_angle_smoothing_factor == 1.0
+
+
+def test_interpolationparameters_constructor_defaults():
+    """Tests the default values after constructing a `InterpolationParameters`."""
+
+    parameters = InterpolationParameters(False, True)
+
+    assert parameters.refine_intersected is False
+    assert parameters.use_mass_center_when_refining is True
+    assert parameters.max_refinement_iterations == 10
+    assert parameters.averaging_method == AveragingMethod.SIMPLE_AVERAGING
+    assert parameters.min_points == 1
+    assert parameters.relative_search_radius == 1.01
+    assert parameters.interpolate_to == InterpolateToOption.ZK
