ff3782ba31dcd5a8da9a54fdd844f1a6d102be34
Prisca van der Sluis <prisca.vandersluis@deltares.nl>
2021-04-29 12:03:45 +0200

    GRIDEDIT-368 Add test `test_cgeometrylist_from_geometrylist` and skeleton tests.

--- tests/test_c_structures.py
+++ tests/test_c_structures.py
@@ -3,8 +3,8 @@ import pytest
 from numpy.ctypeslib import as_array
 from numpy.testing import assert_array_equal
 
-from meshkernel import Mesh2d
-from meshkernel.c_structures import CMesh2d
+from meshkernel import GeometryList, Mesh2d
+from meshkernel.c_structures import CGeometryList, CMesh2d
 
 
 def test_cmesh2d_from_mesh2d():
@@ -81,3 +81,48 @@ def test_cmesh2d_allocate_memory():
     assert mesh2d.edge_y.size == 4
     assert mesh2d.face_x.size == 1
     assert mesh2d.face_y.size == 1
+
+
+def test_cgeometrylist_from_geometrylist():
+    """Tests `from_geometrylist` of the `CGeometryList` class."""
+
+    x_coordinates = np.array([0.0, 2.0, 4.0, 6.0, 8.0], dtype=np.double)
+    y_coordinates = np.array([1.0, 3.0, 5.0, 7.0, 9.0], dtype=np.double)
+    values = np.array([1.0, 2.0, 3.0, 4.0, 5.0], dtype=np.double)
+    geometry_separator = -12.3
+    inner_outer_separator = 45.6
+
+    geometrylist = GeometryList(
+        x_coordinates, y_coordinates, values, geometry_separator, inner_outer_separator
+    )
+
+    cgeometrylist = CGeometryList.from_geometrylist(geometrylist)
+
+    # Get the numpy arrays from the ctypes object
+    cgeometrylist_x_coordinates = as_array(cgeometrylist.x_coordinates, (5,))
+    cgeometrylist_y_coordinates = as_array(cgeometrylist.y_coordinates, (5,))
+    cgeometrylist_values = as_array(cgeometrylist.values, (5,))
+
+    assert_array_equal(cgeometrylist_x_coordinates, x_coordinates)
+    assert_array_equal(cgeometrylist_y_coordinates, y_coordinates)
+    assert_array_equal(cgeometrylist_values, values)
+
+    assert cgeometrylist.geometry_separator == -12.3
+    assert cgeometrylist.inner_outer_separator == 45.6
+    assert cgeometrylist.n_coordinates == 5
+
+
+def test_cgeometrylist_allocate_memory():
+    pass
+
+
+def test_corthogonalizationparameters_from_orthogonalizationparameters():
+    pass
+
+
+def test_cinterpolationparameters_from_interpolationparameters():
+    pass
+
+
+def test_csamplerefineparameters_from_samplerefinementparameters():
+    pass
