80aa5504e145ce457fe216fd558546b4f019bf11
Julian Hofer <julianhofer@gnome.org>
2021-05-06 14:48:44 +0200

    GRIDEDIT-355: Ensuring that the wheel works for all Python versions

--- setup.py
+++ setup.py
@@ -15,13 +15,32 @@ __author__ = ", ".join(author_dict.keys())
 __author_email__ = ", ".join(s for _, s in author_dict.items())
 
 
-def read(rel_path):
+def read(rel_path: str) -> str:
+    """Used to read a text file
+
+    Args:
+        rel_path (str): Relative path to the file
+
+    Returns:
+        str: File content
+    """
     here = os.path.abspath(os.path.dirname(__file__))
     with codecs.open(os.path.join(here, rel_path), "r") as fp:
         return fp.read()
 
 
-def get_version(rel_path):
+def get_version(rel_path: str) -> str:
+    """Get the version string
+
+    Args:
+        rel_path (str): Relative path to the file
+
+    Raises:
+        RuntimeError: Raised if the version string could not be found
+
+    Returns:
+        str: [description]
+    """
     for line in read(rel_path).splitlines():
         if line.startswith("__version__"):
             delim = '"' if '"' in line else "'"
@@ -30,17 +49,46 @@ def get_version(rel_path):
     raise RuntimeError("Unable to find version string.")
 
 
-class BinaryDistribution(Distribution):
-    def has_ext_modules(foo):
-        return True
+def get_meshkernel_name() -> str:
+    """Get the filename of the MeshKernel library
 
+    Raises:
+        OSError: If the operating system is not supported
 
-def get_meshkernel_name():
+    Returns:
+        str: Filename of the MeshKernel library
+    """
     if platform.system() == "Windows":
         return "MeshKernelApi.dll"
     elif platform.system() == "Linux":
         return "libMeshKernelApi.so"
     raise OSError("Unsupported operating system")
+
+
+try:
+    from wheel.bdist_wheel import bdist_wheel as _bdist_wheel
+
+    class bdist_wheel(_bdist_wheel):
+        """Class describing our wheel.
+        Basically it says that it is not a pure Python package,
+        but it also does not contain any Python source and
+        therefore works for all Python versions
+        """
+
+        def finalize_options(self):
+            _bdist_wheel.finalize_options(self)
+            # Mark us as not a pure python package
+            self.root_is_pure = False
+
+        def get_tag(self):
+            python, abi, plat = _bdist_wheel.get_tag(self)
+            # We don't contain any python source
+            python, abi = "py2.py3", "none"
+            return python, abi, plat
+
+
+except ImportError:
+    bdist_wheel = None
 
 
 long_description = read("README.md")
@@ -70,7 +118,7 @@ setup(
     package_data={
         "meshkernel": [get_meshkernel_name()],
     },
-    distclass=BinaryDistribution,
+    cmdclass={"bdist_wheel": bdist_wheel},
     version=get_version("meshkernel/__init__.py"),
     classifiers=["Topic :: Scientific/Engineering :: Mathematics"],
 )
