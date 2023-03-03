abf6ea2dc31b2bf00785b098764dd017d1170d8e
Luca Carniato <luca.carniato@deltares.nl>
2023-03-03 18:09:24 +0100

    Restore

--- scripts/build_deps.sh
+++ scripts/build_deps.sh
@@ -1,13 +1,12 @@
 #build wheels
 PYBIN=/opt/python/cp38-cp38/bin/
 ${PYBIN}/python3 setup.py bdist_wheel
-#cd dist/
-#list=()
-#for file in *linux_x86_64.whl; do
-#    list+=("$file")
-#done
-#auditwheel show ${list[0]}
-#auditwheel repair ${list[0]}
-#cd ..
-#cp ./dist/wheelhouse/*.whl .
-cp ./dist/*.whl .
+cd dist/
+list=()
+for file in *linux_x86_64.whl; do
+    list+=("$file")
+done
+auditwheel show ${list[0]}
+auditwheel repair ${list[0]}
+cd ..
+cp ./dist/wheelhouse/*.whl .
