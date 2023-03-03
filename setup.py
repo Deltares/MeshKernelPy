40f6fc100eeb4fe16e2f015136b5d0d68dbf59a4
Luca Carniato <luca.carniato@deltares.nl>
2023-03-03 22:33:09 +0100

    Add include_package_data

--- setup.py
+++ setup.py
@@ -224,6 +224,7 @@ setup(
     package_data={
         "meshkernel": [get_library_name()],
     },
+    include_package_data=True,
     packages=find_packages(),
     ext_modules=[CMakeExtension("https://github.com/Deltares/MeshKernel")],
     cmdclass={"bdist_wheel": bdist_wheel, "build_ext": build_ext},
