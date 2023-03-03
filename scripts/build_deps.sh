19680ec5c6f52fda6676653cf5c5d6223f65bd06
Luca Carniato <luca.carniato@deltares.nl>
2023-03-03 22:25:20 +0100

    correct

--- scripts/build_deps.sh
+++ scripts/build_deps.sh
@@ -1,7 +1,7 @@
 #build wheels
 PYBIN=/opt/python/cp38-cp38/bin/
-${PYBIN}/python3 setup.py bdist_wheel
 rm -rf build *.egg-info
+${PYBIN}/python3 setup.py bdist_wheel
 cd dist/
 list=()
 for file in *linux_x86_64.whl; do
