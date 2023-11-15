997bfff7ef095aa7adb5df01327f6b81849daecd
Ahmad El Sayed <ahmad.elsayed@deltares.nl>
2023-11-15 12:43:56 +0100

    Added message prior to exiting

--- scripts/build_deps.sh
+++ scripts/build_deps.sh
@@ -1,9 +1,9 @@
 # Build wheels
 PYBIN=/opt/python/cp38-cp38/bin/
 rm -rf build *.egg-info
-${PYBIN}/python3 -m pip install numpy matplotlib type_enforced
-${PYBIN}/python3 setup.py build_ext || exit 1
-${PYBIN}/python3 setup.py sdist bdist_wheel || exit 1
+${PYBIN}/python3 -m pip install numpy matplotlib type_enforced || echo 'build_deps.sh: pip failed' && exit 1
+${PYBIN}/python3 setup.py build_ext || echo 'build_deps.sh: setup.py build_ext failed' && exit 1
+${PYBIN}/python3 setup.py sdist bdist_wheel || echo 'build_deps.sh: setup.py sdist bdist_wheel failed' && exit 1
 cd dist/
 list=()
 for file in *linux_x86_64.whl; do
--- scripts/compile_deps.sh
+++ scripts/compile_deps.sh
@@ -9,7 +9,8 @@ export CXX=/opt/rh/${devtoolset}/root/usr/bin/g++
 # Add cmake
 cd /root
 export CMAKE_VERSION='3.23.1'
-wget https://github.com/Kitware/CMake/releases/download/v${CMAKE_VERSION}/cmake-${CMAKE_VERSION}-linux-x86_64.sh || exit 1
+wget https://github.com/Kitware/CMake/releases/download/v${CMAKE_VERSION}/cmake-${CMAKE_VERSION}-linux-x86_64.sh \
+|| echo 'compile_deps.sh: wget CMake failed' && exit 1
 chmod +x cmake-${CMAKE_VERSION}-linux-x86_64.sh
 mkdir /opt/cmake
 ./cmake-${CMAKE_VERSION}-linux-x86_64.sh --skip-license --prefix=/opt/cmake || exit 1
@@ -17,10 +18,12 @@ mkdir /opt/cmake
 # Add boost
 export BOOST_VERSION='1_81_0'
 export BOOST_VERSION_DOT='1.81.0'
-wget https://boostorg.jfrog.io/artifactory/main/release/${BOOST_VERSION_DOT}/source/boost_${BOOST_VERSION}.tar.gz || exit 1
+wget https://boostorg.jfrog.io/artifactory/main/release/${BOOST_VERSION_DOT}/source/boost_${BOOST_VERSION}.tar.gz \
+|| echo 'compile_deps.sh: wget boost failed' && exit 1
 export LD_LIBRARY_PATH=/usr/local/lib:/usr/lib:/usr/local/lib64:/usr/lib64:$LD_LIBRARY_PATH
 tar -xzf boost_${BOOST_VERSION}.tar.gz
 cd boost_${BOOST_VERSION}
-./bootstrap.sh --with-libraries=filesystem,system || exit 1
+./bootstrap.sh --with-libraries=filesystem,system \
+|| echo 'compile_deps.sh: boostrap boost failed' && exit 1
 ./b2 -j4 cxxflags="-fPIC" runtime-link=static variant=release link=static --prefix=/opt/boost_${BOOST_VERSION} install
 cd ..
