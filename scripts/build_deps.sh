cca3ba7e310f745f5492eab985e071f140467f6c
Ahmad El Sayed <ahmad.elsayed@deltares.nl>
2023-11-20 12:35:15 +0100

    Create conda env in docker config

--- scripts/build_deps.sh
+++ scripts/build_deps.sh
@@ -1,9 +1,19 @@
-#build wheels
-PYBIN=/opt/python/cp38-cp38/bin/
+set -x
+
+echo ${DOCKER_CONDA_ENV}
+
+source activate ${DOCKER_CONDA_ENV}
+
+python --version
+
 rm -rf build *.egg-info
-${PYBIN}/python3 -m pip install numpy matplotlib type_enforced
-${PYBIN}/python3 setup.py build_ext || exit 1
-${PYBIN}/python3 setup.py sdist bdist_wheel || exit 1
+
+python -m pip install setuptools wheel auditwheel
+python -m pip install numpy matplotlib type_enforced
+
+python setup.py build_ext || exit 1
+python setup.py sdist bdist_wheel || exit 1
+
 cd dist/
 list=()
 for file in *linux_x86_64.whl; do
@@ -13,3 +23,5 @@ auditwheel show ${list[0]}
 auditwheel repair ${list[0]}
 cd ..
 cp ./dist/wheelhouse/*.whl .
+
+conda remove -n ${DOCKER_CONDA_ENV} --all
--- scripts/compile_deps.sh
+++ scripts/compile_deps.sh
@@ -1,26 +1,36 @@
+
+set -x
+
 # add development tools
 yum -y install git make wget which bzip2 netcdf centos-release-scl
-yum -y install ${devtoolset}
-scl enable ${devtoolset} bash
-export PATH="/opt/rh/${devtoolset}/root/usr/bin:$PATH"
-export CC=/opt/rh/${devtoolset}/root/usr/bin/gcc 
-export CXX=/opt/rh/${devtoolset}/root/usr/bin/g++
+DEVTOOLSET=devtoolset-11
+yum -y install ${DEVTOOLSET}
+scl enable ${DEVTOOLSET} bash
+export PATH="${PATH}:/opt/rh/${DEVTOOLSET}/root/usr/bin"
+export CC=/opt/rh/${DEVTOOLSET}/root/usr/bin/gcc 
+export CXX=/opt/rh/${DEVTOOLSET}/root/usr/bin/g++
 
-# Add cmake
+# enter root
 cd /root
-export CMAKE_VERSION='3.23.1'
+
+# install CMake
+CMAKE_VERSION='3.23.1'
 wget https://github.com/Kitware/CMake/releases/download/v${CMAKE_VERSION}/cmake-${CMAKE_VERSION}-linux-x86_64.sh || exit 1
 chmod +x cmake-${CMAKE_VERSION}-linux-x86_64.sh
 mkdir /opt/cmake
 ./cmake-${CMAKE_VERSION}-linux-x86_64.sh --skip-license --prefix=/opt/cmake || exit 1
 
-# add boost
-export BOOST_VERSION='1_81_0'
-export BOOST_VERSION_DOT='1.81.0'
+# install boost
+BOOST_VERSION='1_81_0'
+BOOST_VERSION_DOT='1.81.0'
 wget https://boostorg.jfrog.io/artifactory/main/release/${BOOST_VERSION_DOT}/source/boost_${BOOST_VERSION}.tar.gz || exit 1
 export LD_LIBRARY_PATH=/usr/local/lib:/usr/lib:/usr/local/lib64:/usr/lib64:$LD_LIBRARY_PATH
 tar -xzf boost_${BOOST_VERSION}.tar.gz
 cd boost_${BOOST_VERSION}
+BOOST_INSTALL_PREFIX=/opt/boost_${BOOST_VERSION}
 ./bootstrap.sh --with-libraries=filesystem,system || exit 1
-./b2 -j4 cxxflags="-fPIC" runtime-link=static variant=release link=static --prefix=/opt/boost_${BOOST_VERSION} install
+./b2 -j4 cxxflags="-fPIC" runtime-link=static variant=release link=static --prefix=${BOOST_INSTALL_PREFIX} install
+export Boost_INCLUDE_DIR=${BOOST_INSTALL_PREFIX}/include
+
+# leave root
 cd ..
