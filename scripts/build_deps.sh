23a593364061fef4a151181301380870f3119499
Ahmad El Sayed <ahmad.elsayed@deltares.nl>
2023-11-20 12:36:54 +0100

    Do not pip install in build_deps.sh

--- scripts/build_deps.sh
+++ scripts/build_deps.sh
@@ -1,7 +1,7 @@
 # Build wheels
 PYBIN=/opt/python/cp38-cp38/bin/
 rm -rf build *.egg-info
-${PYBIN}/python3 -m pip install numpy matplotlib type_enforced || (echo 'build_deps.sh: pip failed' && exit 1)
+#${PYBIN}/python3 -m pip install numpy matplotlib type_enforced || (echo 'build_deps.sh: pip failed' && exit 1)
 ${PYBIN}/python3 setup.py build_ext || (echo 'build_deps.sh: setup.py build_ext failed' && exit 1)
 ${PYBIN}/python3 setup.py sdist bdist_wheel || (echo 'build_deps.sh: setup.py sdist bdist_wheel failed' && exit 1)
 cd dist/
