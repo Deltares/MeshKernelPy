91ba2fccc24bd2bcc6154a32ccff29e6c3ceb193
Ahmad El Sayed <ahmad.elsayed@deltares.nl>
2023-11-21 17:11:59 +0100

    More CR changes

--- scripts/build_wheel.sh
+++ scripts/build_wheel.sh
@@ -3,26 +3,26 @@
 DOCKER_CONDA_ENV="docker_conda_env"
 
 # create conda env and activate it
-conda create -y --force -n ${DOCKER_CONDA_ENV} python=${PYTHON_VERSION} pip
-source activate ${DOCKER_CONDA_ENV}
+conda create -y --force -n "${DOCKER_CONDA_ENV}" python="${PYTHON_VERSION}" pip
+. activate "${DOCKER_CONDA_ENV}"
 
 remove_conda_env() {
   # deactivate and remove conda env
   conda deactivate
-  conda env remove -n ${DOCKER_CONDA_ENV}
+  conda env remove -n "${DOCKER_CONDA_ENV}"
 }
 
 error() {
   # store last exit code before invoking any other command
-  local EXIT_CODE=$?
+  local EXIT_CODE="$?"
   # print error message
-  echo $(basename "$0"): $1
+  echo "$(basename "$0")": "$1"
   remove_conda_env
   exit $EXIT_CODE
 }
 
 # clean up residual data from previous run
-rm -rf build *.egg-info #dist
+rm -rf ./build ./*.egg-info #./dist
 
 python -m pip install \
   setuptools \
@@ -43,8 +43,8 @@ python setup.py bdist_wheel || error "[setup] Building the wheel failed"
   for file in *linux_x86_64.whl; do
     list+=("$file")
   done
-  auditwheel show ${list[0]}
-  auditwheel repair ${list[0]}
+  auditwheel show "${list[0]}"
+  auditwheel repair "${list[0]}"
 )
 
 cp ./dist/wheelhouse/*.whl .
--- scripts/install_deps.sh
+++ scripts/install_deps.sh
@@ -2,16 +2,16 @@
 
 error() {
   # store last exit code before invoking any other command
-  local EXIT_CODE=$?
+  local EXIT_CODE="$?"
   # print error message
-  echo $(basename "$0"): $1
+  echo "$(basename "$0")": "$1"
   exit $EXIT_CODE
 }
 
 # add development tools
 yum -y install git make wget which bzip2 netcdf centos-release-scl
-yum -y install ${DEVTOOLSET}
-scl enable ${DEVTOOLSET} bash
+yum -y install "${DEVTOOLSET}"
+scl enable "${DEVTOOLSET}" bash
 
 # enter root
 (
@@ -19,28 +19,30 @@ scl enable ${DEVTOOLSET} bash
 
   # install CMake
   CMAKE_SCRIPT=cmake-${CMAKE_VERSION}-linux-x86_64.sh
-  wget https://github.com/Kitware/CMake/releases/download/v${CMAKE_VERSION}/${CMAKE_SCRIPT} \
+  wget https://github.com/Kitware/CMake/releases/download/v"${CMAKE_VERSION}"/"${CMAKE_SCRIPT}" \
     || error "[cmake] ${CMAKE_SCRIPT} download failed"
   CMAKE_INSTALL_PREFIX=/opt/cmake
   mkdir ${CMAKE_INSTALL_PREFIX} || error "[cmake] Creation of ${CMAKE_INSTALL_PREFIX} failed"
-  chmod +x ${CMAKE_SCRIPT} || error "[cmake] Changing the permissions of ${CMAKE_SCRIPT} failed"
-  bash ${CMAKE_SCRIPT} --skip-license --prefix=${CMAKE_INSTALL_PREFIX} || error "[cmake] Installation failed"
+  chmod +x "${CMAKE_SCRIPT}" || error "[cmake] Changing the permissions of ${CMAKE_SCRIPT} failed"
+  bash "${CMAKE_SCRIPT}" --skip-license --prefix=${CMAKE_INSTALL_PREFIX} || error "[cmake] Installation failed"
 
   # install boost
-  BOOST_LIB=boost_${BOOST_VERSION//./_}
-  wget https://boostorg.jfrog.io/artifactory/main/release/${BOOST_VERSION}/source/${BOOST_LIB}.tar.gz \
+  BOOST_LIB=boost_"${BOOST_VERSION//./_}"
+  wget https://boostorg.jfrog.io/artifactory/main/release/"${BOOST_VERSION}"/source/"${BOOST_LIB}".tar.gz \
     || error "[boost] ${BOOST_LIB}.tar.gz download failed"
-  export LD_LIBRARY_PATH=/usr/local/lib:/usr/lib:/usr/local/lib64:/usr/lib64:$LD_LIBRARY_PATH
-  tar -xzf ${BOOST_LIB}.tar.gz || error "[boost] ${BOOST_LIB}.tar.gz extraction failed"
-  cd ${BOOST_LIB}
-  ./bootstrap.sh --with-libraries=filesystem,system || error "[boost] bootstrap failed"
-  ./b2 -j4 cxxflags="-fPIC" runtime-link=static variant=release link=static --prefix=/opt/boost_${BOOST_VERSION} install \
-    || error "[boost] Installation failed"
-
+  export LD_LIBRARY_PATH=/usr/local/lib:/usr/lib:/usr/local/lib64:/usr/lib64:"$LD_LIBRARY_PATH"
+  tar -xzf "${BOOST_LIB}".tar.gz || error "[boost] ${BOOST_LIB}.tar.gz extraction failed"
+  (
+    cd "${BOOST_LIB}" || error "Could not change the directory to ${BOOST_LIB}"
+    ./bootstrap.sh --with-libraries=filesystem,system || error "[boost] bootstrap failed"
+    ./b2 -j4 cxxflags="-fPIC" runtime-link=static variant=release link=static --prefix=/opt/boost_"${BOOST_VERSION}" install \
+      || error "[boost] Installation failed"
+  )
+  
   # install miniconda
   MINICONDA3_SCRIPT=Miniconda3-${MINICONDA3_VERSION}-Linux-x86_64.sh
-  wget https://repo.continuum.io/miniconda/${MINICONDA3_SCRIPT} \
+  wget https://repo.continuum.io/miniconda/"${MINICONDA3_SCRIPT}" \
     || error "[miniconda] ${MINICONDA3_SCRIPT} download failed"
-  chmod +x ${MINICONDA3_SCRIPT} || error "[miniconda] Changing the permissions of ${MINICONDA3_SCRIPT} failed"
-  bash ${MINICONDA3_SCRIPT} -b -p /opt/conda || error '[miniconda] Installation failed'
+  chmod +x "${MINICONDA3_SCRIPT}" || error "[miniconda] Changing the permissions of ${MINICONDA3_SCRIPT} failed"
+  bash "${MINICONDA3_SCRIPT}" -b -p /opt/conda || error '[miniconda] Installation failed'
 )
