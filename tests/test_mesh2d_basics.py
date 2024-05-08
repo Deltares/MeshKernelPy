b4891d9754bc765a012d72e15ca38ebc359f06f1
Luca Carniato <luca.carniato@deltares.nl>
2024-05-08 11:22:49 +0200

    Add int and double types

--- tests/test_mesh2d_basics.py
+++ tests/test_mesh2d_basics.py
@@ -10,6 +10,7 @@ from meshkernel import (
     GeometryList,
     GriddedSamples,
     InputError,
+    InterpolationValues,
     MakeGridParameters,
     Mesh2d,
     MeshKernel,
@@ -1887,7 +1888,21 @@ def test_mesh2d_convert_projection():
     assert mesh2d.almost_equal(mesh2d_final, rtol=0)
 
 
-def test_mesh2d_refine_based_on_gridded_samples_coastline():
+cases_test_mesh2d_refine_based_on_gridded_samples_coastline = [
+    InterpolationValues.SHORT,
+    InterpolationValues.FLOAT,
+    InterpolationValues.INT,
+    InterpolationValues.DOUBLE,
+    4,
+]
+
+
+@pytest.mark.parametrize(
+    "interpolation_type", cases_test_mesh2d_refine_based_on_gridded_samples_coastline
+)
+def test_mesh2d_refine_based_on_gridded_samples_coastline(
+    interpolation_type: InterpolationValues,
+):
     """Tests `mesh2d_refine_based_on_gridded_samples` with real world data"""
 
     # set up
@@ -1932,7 +1947,17 @@ def test_mesh2d_refine_based_on_gridded_samples_coastline():
         ]
     )
 
-    values_np = values_2d.flatten().astype(np.float32)
+    if interpolation_type == InterpolationValues.FLOAT:
+        values_np = values_2d.flatten().astype(np.float32)
+    elif interpolation_type == InterpolationValues.SHORT:
+        values_np = values_2d.flatten().astype(np.int16)
+    elif interpolation_type == InterpolationValues.INT:
+        values_np = values_2d.flatten().astype(np.int64)
+    elif interpolation_type == InterpolationValues.DOUBLE:
+        values_np = values_2d.flatten().astype(np.float64)
+    else:
+        values_np = values_2d.flatten().astype(np.float32)
+
     gridded_samples = GriddedSamples(
         x_coordinates=lon_np, y_coordinates=lat_np, values=values_np
     )
@@ -2085,53 +2110,52 @@ def test_mesh2d_refine_based_on_gridded_samples_coastline():
     assert len(mesh2d.node_x) == 1676
     assert len(mesh2d.edge_nodes) == 7176
 
+
 def test_mesh2d_get_face_polygons():
     """Tests `mesh2d_get_face_polygons` with a real mesh"""
 
-    edge_nodes = np.array([0, 4,
-                           1, 5,
-                           2, 6,
-                           3, 7,
-                           4, 8,
-                           5, 9,
-                           7, 10,
-                           8, 11,
-                           10, 12,
-                           0, 1,
-                           1, 2,
-                           2, 3,
-                           5, 6,
-                           6, 7,
-                           8, 9,
-                           9, 10,
-                           11, 12], dtype=np.int32)
-    node_x = np.array([0,
-                       1,
-                       2,
-                       3,
-                       0,
-                       1,
-                       2,
-                       3,
-                       0,
-                       1,
-                       3,
-                       0,
-                       1], dtype=np.double)
+    edge_nodes = np.array(
+        [
+            0,
+            4,
+            1,
+            5,
+            2,
+            6,
+            3,
+            7,
+            4,
+            8,
+            5,
+            9,
+            7,
+            10,
+            8,
+            11,
+            10,
+            12,
+            0,
+            1,
+            1,
+            2,
+            2,
+            3,
+            5,
+            6,
+            6,
+            7,
+            8,
+            9,
+            9,
+            10,
+            11,
+            12,
+        ],
+        dtype=np.int32,
+    )
+    node_x = np.array([0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 3, 0, 1], dtype=np.double)
 
-    node_y = np.array([0,
-                       0,
-                       0,
-                       0,
-                       1,
-                       1,
-                       1,
-                       1,
-                       2,
-                       2,
-                       2,
-                       3,
-                       3], dtype=np.double)
+    node_y = np.array([0, 0, 0, 0, 1, 1, 1, 1, 2, 2, 2, 3, 3], dtype=np.double)
 
     input_mesh2d = Mesh2d(node_x, node_y, edge_nodes)
     mk = MeshKernel()
@@ -2139,32 +2163,45 @@ def test_mesh2d_get_face_polygons():
 
     output_geometry_list = mk.mesh2d_get_face_polygons(5)
 
-
     # Test if edges are correctly calculated
-    assert_array_equal(output_geometry_list.x_coordinates, np.array([1.0000,
-                                                               2.0000,
-                                                               3.0000,
-                                                               3.0000,
-                                                               1.0000,
-                                                               1.0000,
-                                                               -999.0,
-                                                               0.0000,
-                                                               1.0000,
-                                                               3.0000,
-                                                               1.0000,
-                                                               0.0000,
-                                                               0.0000]))
+    assert_array_equal(
+        output_geometry_list.x_coordinates,
+        np.array(
+            [
+                1.0000,
+                2.0000,
+                3.0000,
+                3.0000,
+                1.0000,
+                1.0000,
+                -999.0,
+                0.0000,
+                1.0000,
+                3.0000,
+                1.0000,
+                0.0000,
+                0.0000,
+            ]
+        ),
+    )
 
-    assert_array_equal(output_geometry_list.y_coordinates, np.array([1.0000,
-                                                               1.0000,
-                                                               1.0000,
-                                                               2.0000,
-                                                               2.0000,
-                                                               1.0000,
-                                                               -999.0,
-                                                               2.0000,
-                                                               2.0000,
-                                                               2.0000,
-                                                               3.0000,
-                                                               3.0000,
-                                                               2.0000]))
+    assert_array_equal(
+        output_geometry_list.y_coordinates,
+        np.array(
+            [
+                1.0000,
+                1.0000,
+                1.0000,
+                2.0000,
+                2.0000,
+                1.0000,
+                -999.0,
+                2.0000,
+                2.0000,
+                2.0000,
+                3.0000,
+                3.0000,
+                2.0000,
+            ]
+        ),
+    )
