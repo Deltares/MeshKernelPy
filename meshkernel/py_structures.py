981a2ecd1da342115af4791782e6a52c8bb58d71
Luca Carniato <luca.carniato@deltares.nl>
2024-09-02 16:09:50 +0200

    Add mesh2d_get_property

--- meshkernel/py_structures.py
+++ meshkernel/py_structures.py
@@ -118,6 +118,12 @@ class Mesh2d:
 
     """
 
+    @unique
+    class Property(IntEnum):
+        """Different properties on a 2D mesh."""
+
+        ORTHOGONALITY = 0,
+        EDGE_LENGTHS = 1
     def __init__(
         self,
         node_x=np.empty(0, dtype=np.double),
