aee870d745e3d66eed13e5bcbdca42d1da08f2cc
Bill Senior <bill.senior@deltares.nl>
2025-11-18 09:54:44 +0100

    GRIDEDIT-2044 Fixed formatting warning

--- tests/test_mesh2d_basics.py
+++ tests/test_mesh2d_basics.py
@@ -146,8 +146,8 @@ def test_mesh2d_set_and_mesh2d_get_with_holes():
         geometry_separator=-999,
     )
 
-    innerPolygon = mk.mesh2d_get_inner_boundary_polygons ();
-    assert innerPolygon.n_coordinates == 32
+    innerPolygon = mk.mesh2d_get_inner_boundary_polygons();
+    assert innerPolygon.n_coordinates == 5
 
 
 def test_mesh2d_add():
