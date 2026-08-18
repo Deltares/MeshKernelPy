254d410058e39c86528688135abfabcdf3325603
Bill Senior <bill.senior@deltares.nl>
2026-08-18 11:17:51 +0200

    GRIDEDIT-2301 Updated comment

--- meshkernel/meshkernel.py
+++ meshkernel/meshkernel.py
@@ -645,7 +645,7 @@ class MeshKernel:
 
     def mesh2d_make_global(self, num_longitude_nodes: int) -> None:
         """Compute the global mesh with a given number of points along the longitude direction.
-        The points in the latitude direction will continue to the poles.
+        The points in the latitude direction will continue up to the poles.
 
         Args:
         num_longitude_nodes (int): The number of points along the longitude.
